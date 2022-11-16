1a5361dae33699be136c362644d3018f629402ea
ahmergit <117104570+ahmergit@users.noreply.github.com>
2022-11-16 17:08:29 +0500

    Add files via upload

---
+++ assignment 2/app.js
@@ -0,0 +1,301 @@
+// Chapter 1 Alert
+// alert("welcome to javascript")
+// alert("Error!Please enter a valid password")
+// alert("Welcome to JS Land \n Happy coding ")
+
+
+
+
+
+// Chapter 2 Variable
+
+// var username = "Hammad";
+// console.log(username);
+
+// var myName="Hammad Imran"
+// console.log(myName);
+
+// var message = "Hello World"
+// alert(message);
+
+// var Studentname ="hammad";
+// var age = 15;
+// var course = "Web developmet"
+//  alert(Studentname)
+//  alert(age)
+//  alert(course)
+
+//  var a = "pizza"
+//  var b = "pizz"
+//  var c = "piz"
+//  var d = "pi"
+//  var e = "p"
+
+//  alert("pizza\npizz\npiz\npi\np");
+
+//  var email = "example@example.com"
+
+//  alert( 'my email addres is'+ email);
+
+//  var msg = "yah!i can write html content through js"
+
+//  document.write(msg)
+
+// var icon = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(icon)
+
+
+
+
+
+// chapter 3 variables for number
+
+// var age = 15;
+// alert("i am "+age+" years old")
+
+
+
+
+
+
+//chapter 5 Math Expressions
+
+
+// var a 
+// value after var declairation
+// a = 13;
+// initial value
+// a++;
+// value after increment is 14
+
+// a=a+11;
+
+// a--;
+
+// var b;
+
+// b=a%4;
+
+// console.log("remainder is " + b);
+
+
+
+// 4.
+
+// var ticket =600;
+
+// total=ticket*5;
+
+// console.log("Total cost to buy 5 ticket to a movie is "+total);
+
+
+// Chapter 9-11
+
+
+// var city =prompt();
+// if("Karachi"==city){
+// console.log("Welcome to City of lights");
+
+// }
+// else{
+//     console.log("welcome to lahore");
+// }
+
+
+
+// var light=prompt("signal light");
+
+// if("red"==light){
+// console.log("Stop your vehicle");
+
+// }
+// else if("yellow"==light){
+// console.log("Be ready");
+// }
+// else if("green"==light){
+//     console.log("now move");
+// }
+// else{
+//     console.log("dont stop your vehicle");
+// }
+
+
+
+
+
+// var op=prompt("operator");
+// var fn=prompt("n1");
+// var sn=prompt("n2");
+// if(op=="+"){
+//  t = fn+sn; 
+
+// }
+// if(op=="+"){
+//  t = fn+sn; 
+//  console.log(t);
+
+// }
+
+// if(op=="-"){
+//     t = fn-sn; 
+//     console.log(t);
+//    }
+//    else if(op=="*"){
+//     t = fn*sn; 
+//     console.log(t);
+//    }
+//    else if(op=="/"){
+//     t = fn/sn; 
+//     console.log(t);
+//    }
+         
+//    else if(op=="%"){
+//     t = fn%sn; 
+//     console.log(t);
+//    }
+
+// else{
+//     console.log("error");
+// }
+ 
+
+
+   
+
+
+// Chapter 12-13
+
+
+
+
+
+// var time;
+// time=prompt("time")
+
+// if(time>1&&time<12);
+// {
+// console.log("its morning");
+// }
+// if(time<19&&time>12){
+// console.log("its evening");
+// }
+// if(time<24&&time>19){
+//     console.log("its night");
+// }
+// else{
+//     console.log("time is not set");
+// }
+
+
+
+
+
+
+
+
+// Array
+
+
+// var Sname=[];
+
+// Sname[0]="DAni"
+// Sname[1]="DAn"
+// Sname[2]="DAi"
+// Sname[3]="Dni"
+// alert(Sname[2])
+
+
+
+
+
+// var Array=["you","are","num",1]
+
+
+// alert(Array);
+
+
+
+
+
+// var S=["ham","Sam","Dan"]
+
+// var sc=["331","292","275"]
+
+// var pr=["82%","79%","74%"]
+
+// alert("Score of "+S[0]+" is "+sc[0]+" and per is "+pr[0])
+
+
+
+// var color=["red","black"]
+//  color.unshift("blue");
+ 
+//  color.push("white")
+//  color.shift()
+//  color.pop()
+//  color.splice(1,1,"voilet","purple")
+//  alert(color);
+
+
+
+// var score=[41,25,65,22]
+
+// alert(score)
+
+// score.sort();
+// alert(score)
+
+
+// var cities=[];
+// cities[0]="nairobi"
+// cities[1]="naiobi"
+// cities[2]="nirobi"
+// cities[3]="naibi"
+// alert("cities are : "+cities);
+
+// selected=cities.slice(1,3);
+// alert("selected cities are : " +selected) ;
+    
+
+
+
+
+
+
+
+
+// chapter 17-20
+
+
+
+// for(var i=0; i<=10;i++){
+
+// console.log(i);
+// }
+
+
+// var i =prompt("Table number");
+// var j =prompt("Lenght of the Table");
+// var result;
+// for(var s=0;s<=j;s++){
+// result=i*s;
+//     console.log( + i + " * " + s + " : "+result);
+
+
+// }
+
+
+
+// for(var i=0;i<=10;i++){
+//     if(i%2==0){
+//         alert(i)
+//     }
+
+// }
+
+// for(var i=0;i<=10;i++){
+//     if(i%2==1){
+//         alert(i)
+//     }
+
+// }
